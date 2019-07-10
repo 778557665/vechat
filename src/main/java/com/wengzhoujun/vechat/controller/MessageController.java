dc967e0b5be1d07a7e2d14b1556811249e295963
翁周俊 <wengzhoujun@zhibitech.com>
2019-07-10 11:25:24 +0800

    配置修改 接口修改

--- src/main/java/com/wengzhoujun/vechat/controller/MessageController.java
+++ src/main/java/com/wengzhoujun/vechat/controller/MessageController.java
@@ -8,10 +8,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created on 2019/7/10.
@@ -27,14 +26,31 @@ public class MessageController {
     private SocketIOService socketIOService;
 
     @ApiOperation(value = "发送消息至用户")
-    @PostMapping("/pushMessage")
-    public Result pushMessage(@ApiParam(value = "接收消息的用户id") @RequestParam Long userId,
+    @PostMapping("/push")
+    public Result pushMessage(@ApiParam(value = "接收消息的用户id") @RequestParam String userId,
                               @ApiParam(value = "消息内容") @RequestParam String content) {
-        String userIdStr = userId.toString();
         Message message = new Message();
-        message.setId(userIdStr);
+        message.setId(userId);
         message.setContent(content);
         socketIOService.pushMessageToUser(message);
+        return ResponseUtil.ok();
+    }
+
+    /**
+     * 推送消息给所有客户端
+     * @param content	消息内容
+     * @return
+     */
+    @ApiOperation(value = "发送消息至多个用户")
+    @PostMapping("/pushAll")
+    public Result pushAllMessage(@ApiParam(value = "接收消息的用户ids") @RequestBody List<String> ids,
+                                 @ApiParam(value = "消息内容") @RequestParam String content) {
+        ids.forEach(c -> {
+            Message message = new Message();
+            message.setId(c);
+            message.setContent(content);
+            socketIOService.pushMessageToUser(message);
+        });
         return ResponseUtil.ok();
     }
 
